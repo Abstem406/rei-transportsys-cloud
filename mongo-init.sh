bffe7bd58d1c12da11d6c8881365813e2302a270
Abstem406 <pauul406@gmail.com>
2025-08-06 20:32:27 -0400

    fix: usa BD transportsys y auto-restore dump

--- mongo-init.sh
+++ mongo-init.sh
@@ -2,10 +2,10 @@
 # mongo-init.sh
 set -e
 
-echo "🔍 Buscando dump en /docker-entrypoint-initdb.d/dump/transportsys"
-if [ -d "/docker-entrypoint-initdb.d/dump/transportsys" ]; then
+echo "🔍 Buscando dump en /docker-entrypoint-initdb.d/dump"
+if [ -d "/docker-entrypoint-initdb.d/dump" ]; then
   echo "🚀 Restaurando base de datos 'transportsys'..."
-  mongorestore --drop --db transportsys /docker-entrypoint-initdb.d/dump/transportsys
+  mongorestore --drop --db transportsys /docker-entrypoint-initdb.d/dump
   echo "✅ Restauración terminada"
 else
   echo "⚠️  No se encontró el dump; se omite la restauración"
