7ca2c4922401ff1a36982cce133c2163df5cd55f
Abstem406 <pauul406@gmail.com>
2025-08-06 23:09:48 -0400

    fix: usa BD transportsys y auto-restore dump

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo ">>> Waiting for MongoDB to start..."
-until mongosh --eval "print(\"waited for connection\")" 2>/dev/null; do
-    sleep 1
-done
-
-echo ">>> Restoring database..."
-mongorestore --db transportsys /docker-entrypoint-initdb.d/dump
-echo ">>> Restore complete!"
+echo ">>> Restaurando base de datos..."
+mongorestore --db transportsys --drop /docker-entrypoint-initdb.d/dump
+echo ">>> Restauración completada!"
